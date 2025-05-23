89d20a1365d2d9509c472a532eb8a069a3389742
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-05-24 01:43:45 +0530

    Integer Hashing using Map

---
+++ Hashing/IntegerMap.java
@@ -0,0 +1,20 @@
+package Hashing;
+
+import java.util.*;
+
+public class IntegerMap {
+    static void mapInteger(int arr[]) {
+        Map<Integer, Integer> mapList = new HashMap<>();
+        int hashArr[] = new int[13];
+
+        for (int j = 0; j < arr.length; j++)
+            mapList.put(arr[j], ++hashArr[arr[j]]);
+
+        System.out.println(mapList);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 1, 6, 4, 2, 2, 1, 1, 12 };
+        mapInteger(arr);
+    }
+}
