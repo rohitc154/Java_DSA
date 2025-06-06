fd421cb5263793356cf45d1fd521cb3db7b1b02f
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-06-06 21:58:17 +0530

    Array Rotation

---
+++ Array/RightRotateCollection.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RightRotateCollection {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        int rotateBy = 3;
+
+    }
+}
