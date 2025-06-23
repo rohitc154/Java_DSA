46f34095703bd609b0093899b277b48a91823f09
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-06-23 11:52:31 +0530

    Searching in rotated array(duplicate)

---
+++ BinSearch/SearchRotatedArr2.java
@@ -0,0 +1,34 @@
+public class SearchRotatedArr2 {
+    public static boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            } else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 5, 6, 0, 0, 1, 2 };
+        boolean result = search(arr, 8);
+        System.out.println(result);
+    }
+}
