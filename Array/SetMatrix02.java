442ff7e1c51dae43105644f4e827376c048e33d7
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-07-17 16:55:23 +0530

    Binary Matrix Prob stage 02

---
+++ Array/SetMatrix02.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class SetMatrix02 {
+    public static void main(String[] args) {
+
+        int binArr[][] = { { 1, 1, 1, 1 }, { 1, 0, 1, 1 }, { 1, 1, 0, 1 }, { 1, 0, 0, 1 } };
+        int row = binArr[0].length;
+        int col = binArr.length;
+
+        int rowArr[] = new int[row];
+        int colArr[] = new int[col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (binArr[i][j] == 0) {
+                    rowArr[i] = 1;
+                    colArr[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < colArr.length; i++) {
+            System.out.println(rowArr[i] + "  " + colArr[i]);
+        }
+    }
+}
